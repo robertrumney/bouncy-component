4845919917604c2d13f675c7547650aa42bf7907
Robert Rumney <robertrumney@gmail.com>
2023-05-25 13:17:01 +0200

    Create BouncyBallComponent.h

---
+++ BouncyBallComponent.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "BouncyBallComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class YOURGAME_API UBouncyBallComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this component's properties
+    UBouncyBallComponent();
+
+    // Called when the game starts
+    virtual void BeginPlay() override;
+
+protected:
+    UPROPERTY(EditAnywhere, Category = "Physics")
+    float Restitution;
+
+    UPROPERTY(EditAnywhere, Category = "Physics")
+    float Friction;
+
+private:
+    void MakeBouncy();
+};
